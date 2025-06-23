ae2f284e676ecce3c91a90877e66813b837a85ff
Jaehun <sja06006@gmail.com>
2025-06-23 12:25:17 +0900

    250623Lecture

---
+++ study/250623/1.js
@@ -0,0 +1,6 @@
+let name = prompt("이름을 입력하세요");
+console.log("안녕하세요, "+ name + "님!");
+
+// let a=10;
+// let b=3;
+// console.log("a+b=" + a + b);
---
+++ study/250623/main.js
@@ -0,0 +1,9 @@
+document.write("<h1>Document Write First</h1>")
+alert("Hello, World!!!");
+console.log("자바스크립트는 객체 기반입니다.");
+console.log("데이터 타입 선언 없이 변수 생성 가능!");
+console.log("1 + 2 = ", 1 + 2); 
+document.write("<h1>Document Write Last</h1>");
+let name = prompt("당신의 이름은?");
+alert("안녕하세요, " + name + "님!");
+document.write("안녕하세요, " + name + "님!");
