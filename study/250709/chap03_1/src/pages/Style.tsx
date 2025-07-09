0ee2d9f14b0be980a273342a6c0160fcac748b04
Jaehun <sja06006@gmail.com>
2025-07-09 16:24:39 +0900

    .

---
+++ study/250709/chap03_1/src/pages/Style.tsx
@@ -0,0 +1,7 @@
+export default function Style() {
+    return <div>
+        <h3>Style</h3>
+        <span className="material-icons" style={{color: 'blue', fontSize: '250px'}}>home</span>
+        <span className="material-icons" style={{color: 'green', fontSize: '100px'}}>check_circle_outline</span>
+    </div>;
+}
