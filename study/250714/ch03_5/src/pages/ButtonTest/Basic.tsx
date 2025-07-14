a6006c1bedf2a0641d85cdc597717abb0d3f2de9
Jaehun <sja06006@gmail.com>
2025-07-14 16:28:39 +0900

    .

--- study/250714/ch03_5/src/pages/ButtonTest/Basic.tsx
+++ study/250714/ch03_5/src/pages/ButtonTest/Basic.tsx
@@ -5,8 +5,8 @@ export default function Basic() {
     <section className="mt-4">
         <h2 className="text-5xl font-bold text-center">Basic</h2>
         <div className="flex mt-4 justify-evenly">
-            <button className="btn btn-primary">DAISYUI BUTTON</button>
-            <Button className="btn btn-primary">BUTTON</Button>
+            <button className="btn-primary">DAISYUI BUTTON</button>
+            <Button className="btn-primary">BUTTON</Button>
         </div>
     </section>);
 }
