82d20ae23737befe3e6bc678d589709f24083844
Jaehun <sja06006@gmail.com>
2025-07-15 11:32:33 +0900

    Algorithm Practice

--- 프로그래머스/training_wear.js
+++ 프로그래머스/training_wear.js
@@ -12,31 +12,39 @@
 // 0<lost.length<n+1
 // 여벌학생이 도난시 대여 불가
 
+// !!! 문제 조건에 배열들이 정렬되어 있다는 보장이 없었음.
+// 순서가 중요한 경우 배열을 반드시 정렬해주기
+
 function solution(n, lost, reserve) {
     var answer = 0;
-    // let numLost = lost.length; //잃어버린 사람 수
-    // let numReserve = reserve.length; //빌려줄 수 있는 사람 수
-    // let duplication = 0; //여분이 있으나 도난당한 사람 수
-    let check = [[]]; //lost의 앞뒤값 확인 후 기록을 위한 2차원 배열
-    // reserve 배열에서 lost배열과 중복되는 값 제거하기
-    for (let i=0; i<reserve.length; i++) {
-        if (lost.find(num => {num === reserve[i]}) !== undefined) {
-            reserve.filter((num) => {num !== reserve[i]});
+    let stupid = [];
+    let safe = [];
+    // reserve 배열과 lost배열에서 중복되는 값 제거하기
+
+    lost.forEach(element => {
+        if (reserve.includes(element)) {
+            stupid.push(element);
+            reserve = reserve.filter((num) => {return num !== element});
         }
-    }
+    });
+    lost = lost.filter(num => !stupid.includes(num));
+    lost = lost.sort((a, b) => a - b);
+
     // lost배열을 돌며 reserve 배열의 본인 앞 뒤 번호가 있는지 확인
     for (let i=0; i<lost.length; i++) {
         // 본인 앞 번호 확인
         if (reserve.includes(lost[i] - 1)) {
-            check[i][0] = lost[i] - 1;
+            safe.push(lost[i]);
+            reserve = reserve.filter(num => num !== (lost[i] - 1));
         }
         // 본인 뒷 번호 확인
-        if (reserve.includes(lost[i] + 1)) {
-            check[i][1] = lost[i] + 1;
+        else if (reserve.includes(lost[i] + 1)) {
+            safe.push(lost[i]);
+            reserve = reserve.filter(num => num !== (lost[i] + 1))
         }
     }
-    // check 배열에서 check[i][1]과 check[i+1][0] 간의 중복 요소 확인
-    
 
+    answer = n - (lost.filter(num => !safe.includes(num)).length)
+    
     return answer;
 }
