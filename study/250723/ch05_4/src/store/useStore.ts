dd15f1b0e21a7aea518a9184c1d37c2977cd04d7
Jaehun <sja06006@naver.com>
2025-07-23 16:07:33 +0900

    .

--- study/250723/ch05_4/src/store/useStore.ts
+++ study/250723/ch05_4/src/store/useStore.ts
@@ -2,18 +2,17 @@ import {configureStore} from '@reduxjs/toolkit'
 import {useMemo} from 'react'
 import {rootReducer} from './rootReducer'
 import logger from 'redux-logger'
-import {thunk} from 'redux-thunk'
 
 const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializeStore = () => {
-  const middleware: any[] = [thunk]
-  if (useLogger) {
-    middleware.push(logger)
-  }
   const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(middleware)
+    middleware: getDefaultMiddleware => {
+      const middleware = getDefaultMiddleware()
+      if (useLogger) middleware.push(logger)
+      return middleware
+  }
   })
   return store
 }
