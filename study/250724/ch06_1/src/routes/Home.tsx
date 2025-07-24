f5af02591457addc5653fccaeee149eecae9097a
sja06006 <sja06006@naver.com>
2025-07-24 14:41:32 +0900

    250724 Lecture

---
+++ study/250724/ch06_1/src/routes/Home.tsx
@@ -0,0 +1,21 @@
+import type { FC } from "react"
+import { Link } from "react-router-dom"
+
+type HomeProps = {
+  title?: string
+}
+
+const Home: FC<HomeProps> = ({title}) => {
+  return (
+    <div>
+      <div className="flex p-4 bg-gray-200">
+        <Link to="/">Home</Link>
+        <Link to="/welcome" className="ml-4">Welcome</Link>
+        <Link to="/board" className="ml-4">Board</Link>
+      </div>
+      <p>{title ?? 'Home'}</p>
+    </div>
+  )
+}
+
+export default Home
