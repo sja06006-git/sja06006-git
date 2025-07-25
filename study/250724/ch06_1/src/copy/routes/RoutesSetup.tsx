1e0b8f35957d81317e75dc33bac3e931822c475a
sja06006 <sja06006@naver.com>
2025-07-25 14:13:25 +0900

    250724 Lecture

---
+++ study/250724/ch06_1/src/copy/routes/RoutesSetup.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+import NoMatch from "./NoMatch"
+
+export default function RoutesSetup() {
+  return (
+    <Routes>
+      {/* <Route path="" element={}/> */}
+      <Route path="*" element={<NoMatch />}/>
+    </Routes>
+  )
+}
