726d0c461d658fccfb42097fa8b8828f3a90094a
sja06006 <sja06006@naver.com>
2025-07-28 10:04:37 +0900

    .

--- study/250725/ch06_2/src/routes/Layout/NavigationBar.tsx
+++ study/250725/ch06_2/src/routes/Layout/NavigationBar.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
+import { Link } from "../../components"
 
 export default function NavigationBar() {
   return (
-    <div className="flex p-2 navbar bg-base-100">
+    <div className="flex p-4 bg-gray-200">
       {/* <Link to='/'></Link> */}
-      <Link to='/'>Home</Link>
-      <Link to='/board' className="ml-4">Board</Link>
+      <Link to='/' className="btn btn-link">Home</Link>
+      <Link to='/board' className="ml-4 btn btn-link">Board</Link>
     </div>
   )
 }
